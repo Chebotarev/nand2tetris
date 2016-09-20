724e10ab0d6c84aea7157cf84004f8b7348dacc6
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2016-09-20 12:55:37 -0700

    Finish Ch 4 exercises

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,57 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+  // Initialize i to start at the first address of the screen
+  @SCREEN
+  D=A
+  @i
+  M=D
+
+  // Set the entire screen to whatever is stored at @color (default 0)
+  (SCREENLOOP)
+    // Constant that reprsents the end of the screen (and last address)
+    @24576
+    D=A
+    @i
+    D=D-M
+    @ENDSCREENLOOP
+    D;JEQ
+    // End the loop if we've reached the end of the screen
+    @color
+    D=M
+    @i
+    // Manually set the A register (i stores the address of the next screen
+    // pixel to set)
+    A=M
+    M=D
+    @i
+    M=M+1
+    @SCREENLOOP
+    0;JMP
+  (ENDSCREENLOOP)
+
+  // Check for a value at the KBD address
+    // If 0, jump to WHITE (no key pressed)
+    // else jump to BLACK (key pressed)
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  0;JMP
+(BLACK)
+  // Set @color to (-1)_10 = (1111111111111111)_2
+  @color
+  M=-1
+  // Jump to start
+  @LOOP
+  0;JMP
+(WHITE)
+  // Set @color to (0)_10 = (0000000000000000)_2
+  @color
+  M=0
+  // Jump to start
+  @LOOP
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+@R1
+D=M
+@i
+M=D+1
+
+(LOOP)
+  @i
+  M=M-1
+  D=M
+  @END
+  D;JLE
+  @R0
+  D=M
+  @R2
+  M=D+M
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
